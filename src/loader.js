8189026d7e58ee8751631eefdab7af2791ebb703
James Hibben <jmhibben@gmail.com>
2015-05-07 13:51:46 -0500

    Added app menu; more work on file loading

--- src/loader.js
+++ src/loader.js
@@ -2,6 +2,8 @@
 var app = require('app');
 var browserWindow = require('browser-window');
 var ipc = require('ipc');
+var Menu = require('menu');
+var fs = require('fs');
 // access to filesystem through node.js
 // var fs = require('fs');
 // markdown-it's plugin loader expects objects built from plugins via require()
@@ -23,29 +25,68 @@ var mainWindow = null;
 
 function getFileContents(path)
 {
-  var buf = fs.readFile(path);
-  return buf.toString();
+  var buf;
+  fs.readFile(path, function(err, data){
+    if(err) throw err;
+    buf = data;
+  });
+  return buf;
 }
 
-/* The next "function" expects "arg" to be an object of the following format:
- * 'message'  : Contains info on what needs to be done
- * 'path'     : filesystem path to the default directory  NOTE: need to set the default path later
- * 'file'     : The file location (relative to 'path')
- * 'content'  : The contents of any file to be read/written
- *
- * More properties will be added as necessary
- */
-ipc.on('asynchronous-message', function(event, arg)
-{
-  console.log(arg);
-  var content='';
-  if(arg['message'] == 'load')
+/* Menu! */
+var menuTemplate = [
   {
-    content = getFileContents(path+file);
+    label:    'File',
+    submenu:  [
+      {
+        label:      'Load...',
+        sublabel:   'Load a file from the computer',
+        accelerator:'CmdOrCtrl+L'
+      },
+      {
+        label:      'Save',
+        enabled:    false,
+        accelerator:'CmdOrCtrl+S'
+      },
+      {
+        label:      'Save as...',
+        sublabel:   'Save the file under a different name',
+        enabled:    false,
+        accelerator:'CmdOrCtrl+Shift+S'
+      },
+      {
+        type:       'separator'
+      },
+      {
+        label:      'Quit',
+        accelerator:'CmdOrCtrl+Q'
+      }
+    ]
+  },
+  {
+    label:    'Dev',
+    submenu:  [
+      {
+        label:      'Toggle DevTools',
+        sublabel:   'Expose Chrome\'s Developer Tools for the application',
+        accelerator:'CmdOrCtrl+Shift+I',
+        click:      function() { browserWindow.getFocusedWindow().toggleDevTools(); }
+      }
+    ]
+  },
+  {
+    label:    'MuD Notes',
+    submenu:  [
+      {
+        label:      'CommonMark Help'
+      },
+      {
+        label:      'About'
+      }
+    ],
   }
-  var ret = {'message': 'load', 'content': content};
-  event.sender.send('asynchronous-reply', ret);
-});
+];
+var menu = Menu.buildFromTemplate(menuTemplate);
 
 // quit when all windows are closed
 app.on('window-all-closed', function()
@@ -57,6 +98,8 @@ app.on('window-all-closed', function()
 // done initializing; set up and initialize windows
 app.on('ready', function()
 {
+  // set the menu
+  Menu.setApplicationMenu(menu);
   // create browser window
   mainWindow = new browserWindow({width: 1024, height: 768});
   // load the front page
@@ -66,5 +109,27 @@ app.on('ready', function()
   {
     // modify this to deal with multiple pages if/when multi-window support is added
     mainWindow = null;
+  });
+  /* The next "function" expects "arg" to be an object of the following format:
+   * 'message'  : Contains info on what needs to be done
+   * 'path'     : filesystem path to the default directory  NOTE: need to set the default path later
+   * 'file'     : The file location (relative to 'path')
+   * 'content'  : The contents of any file to be read/written
+   *
+   * More properties will be added as necessary
+   */
+  ipc.on('asynchronous-message', function(event, arg)
+  {
+    // console.log(arg['message']);
+    var content='';
+    if(arg['message'] == 'load')
+    {
+      console.log('path: ' + arg['path']);
+      console.log('file: ' + arg['file'])
+      content = getFileContents(arg['path']+arg['file']);
+      // console.log(content);
+    }
+    var ret = {'message': 'load', 'content': content};
+    event.sender.send('asynchronous-reply', ret);
   });
 });
